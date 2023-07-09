bef9b989f6216606727859eb4b5b1430a2a584ec
poykung <Thaweekhun.21@gmail.com>
2023-07-10 02:38:25 +0700

    calculator-app

---
+++ calculator-app/src/App.js
@@ -0,0 +1,11 @@
+import Calculator from "./components/Calculator";
+
+function App() {
+  return (
+    <div className="App">
+      <h1><Calculator/></h1>
+    </div>
+  );
+}
+
+export default App;
